cc74a6b91cf38fd2e8d99b858decefd5f4056fed
Artiom_Sokolov <sokolova-net@mail.ru>
2022-08-12 00:21:10 +0300

    section page__how

--- src/js/app.js
+++ src/js/app.js
@@ -165,7 +165,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 // Функционал добавления классов к хедеру при прокрутке
 // Документация: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
-// flsScroll.headerScroll();
+flsScroll.headerScroll();
 
 // Функционал липкого блока
 // flsScroll.stickyBlock();
